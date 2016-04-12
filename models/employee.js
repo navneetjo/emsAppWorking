a4028a29188488103fd2914d7587a9d8620c5fb0
navneetjo <joshinavneet13@gmail.com>
2016-04-12 15:36:51 +0530

    Update employee.js

--- models/employee.js
+++ models/employee.js
@@ -1,11 +1,17 @@
 var mongoose=require('mongoose');
 var Schema=mongoose.Schema;
  
-var empSchema = new Schema({
-    code : Number,
-  name : String,
-city: String
+ 
+ 
+var empSchema = new Schema({   
+    code : Number,   
+  name : String,   
+city: String    
  
 });
  
-module.exports = mongoose.model('employees', empSchema);
+ 
+ 
+ 
+module.exports = mongoose.model('employees', empSchema);   
+    
